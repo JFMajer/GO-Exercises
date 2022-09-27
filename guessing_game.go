bbd16e9174bab7e34d0451d6ed52f3f6b5076c36
JFMajer <jakubmajer@live.com>
2022-09-27 17:33:27 +0200

    added for loop to allow 10 guesses

--- guessing_game.go
+++ guessing_game.go
@@ -15,18 +15,31 @@ func main() {
 	seconds := time.Now().Unix()
 	rand.Seed(seconds)
 	ran := rand.Intn(100) + 1
-	fmt.Printf("Random number is %v\n", ran)
+	//fmt.Printf("Random number is %v\n", ran)
 
-	fmt.Print("Please input your guess: ")
-	reader := bufio.NewReader(os.Stdin)
-	input, err := reader.ReadString('\n')
-	input = strings.TrimSpace(input)
-	if err != nil {
-		log.Fatal(err)
+	for i := 9; i >= 0; i-- {
+		fmt.Print("Please input your guess: ")
+		reader := bufio.NewReader(os.Stdin)
+		input, err := reader.ReadString('\n')
+		input = strings.TrimSpace(input)
+		if err != nil {
+			log.Fatal(err)
+		}
+		guess, err := strconv.Atoi(input)
+		if err != nil {
+			log.Fatal(err)
+		}
+		if guess < ran {
+			fmt.Println("Your guess was too low")
+			fmt.Printf("You have %v guesses left\n", i)
+		} else if guess > ran {
+			fmt.Println("Your guess was too high")
+			fmt.Printf("You have %v guesses left\n", i)
+		} else {
+			fmt.Println("You guessed correctly!")
+			fmt.Printf("It took you %v guesses\n", 10-i)
+			break
+		}
 	}
-	guess, err := strconv.Atoi(input)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(guess)
+
 }
