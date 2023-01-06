68ead5c29fc9137d2f6c915b45089742af66842e
Majer <jakubmajer@live.com>
2023-01-06 09:15:45 +0100

    restore main.go

--- operators/main.go
+++ operators/main.go
@@ -24,4 +24,20 @@ func main() {
 
 	half := 3 / 2.0
 	fmt.Println(half)
+	goodThreeSquared := math.Pow(3, 2)
+	fmt.Println(goodThreeSquared)
+
+	remainder := 50 % 3
+	fmt.Println(remainder)
+
+	// precedence
+	a := 12.0 * 3.0 / 4.0
+	b := 12.0 * (3.0 / 4.0)
+	c := (12.0 * 3.0) / 4.0
+	fmt.Println(a, b, c)
+
+	f := 12.0 / 3.0 / 4.0
+	fmt.Println(f)
+	f = 12.0 / (3.0 / 4.0)
+	fmt.Println(f)
 }
